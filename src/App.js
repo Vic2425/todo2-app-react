9a3fdf1255ebf3aaf71e6487c7a88a79477c31c5
Vic <vicjs612@gmail.com>
2023-03-02 20:46:37 -0600

    add delete button to todoApp

--- src/App.js
+++ src/App.js
@@ -7,23 +7,51 @@ class App extends Component {
     super();
 
     this.state = {
-      isClicked: false,
-      todos: [],
+      isClicked: false, 
+      todos: [], 
       text: ''
-    }
-  }
+    };
+  };
+
+  onClickHandler = (e) => {
+    e.preventDefault()
+    const toDos = this.state.todos.slice();
+    toDos.push(this.state.text)
+
+    this.setState({
+      todos: toDos,
+      text: " ",
+    });
+  };
+
+  onChangeHandler = (e) => {
+    this.setState({
+      ...this.state,
+      text : e.target.value
+    });
+  };
+
+  handlerDelete = (index) => {
+    this.setState({...this.state,
+      todos : this.state.todos.filter((todo, idx) => idx !== index)
+    });
+  };
 
   render() {
-
     return (
       <div className="App">
         <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         
+          <img src={logo} className="App-logo" alt="logo" />
+          <form onSubmit={this.onClickHandler}>
+            <input type='text' name='text' value={this.state.text} onChange={this.onChangeHandler}></input>
+            <button type='submit' >Add Todo</button>
+          </form>
+          {!!this.state.todos.length && this.state.todos.map((todo, index) => (
+          <p key={index}>{todo} <button onClick={() => this.handlerDelete(index)}>Delete</button></p>))}
         </header>
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
